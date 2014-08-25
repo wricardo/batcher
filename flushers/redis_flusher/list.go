1753406257739a836af440523a23c4d3f4ac1172
Wallace Ricardo <wallace.crd@gmail.com>
2014-08-24 21:41:56 -0400

    Redis List Flusher

---
+++ flushers/redis_flusher/list.go
@@ -0,0 +1,30 @@
+package redis_flusher
+import(
+	"github.com/wricardo/batcher"
+	"github.com/garyburd/redigo/redis"
+//	"time"
+//	"errors"
+)
+
+type RedisListFlusher struct {
+	List string
+}
+
+func (this RedisListFlusher) Flush(conn redis.Conn, f batcher.Flushable) {
+	strings := f.Strings()
+	commands := make([]interface{}, len(strings)+1)
+	commands[0] = this.List
+	for k, v := range strings {
+		commands[k+1] = v
+	}
+	_, err := conn.Do("rpush", commands...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NewListFlusher(list_name string, flush_buffer_size int, pool RedisPool) batcher.Flusher{
+	rlf := RedisListFlusher{List: list_name}
+	rf := NewRedisFlusher(pool, rlf)
+	return batcher.NewDefaultFlusher(flush_buffer_size, rf)
+}
---
+++ flushers/redis_flusher/redis_flusher.go
@@ -0,0 +1,33 @@
+package redis_flusher
+import(
+	"github.com/wricardo/batcher"
+	"github.com/garyburd/redigo/redis"
+//	"time"
+//	"errors"
+)
+
+type RedisFlusherImplementation interface {
+	Flush(redis.Conn, batcher.Flushable)
+}
+
+type RedisFlusher struct {
+	redis_pool     RedisPool
+	implementation RedisFlusherImplementation
+}
+
+func NewRedisFlusher(redis_pool RedisPool, implementation RedisFlusherImplementation) *RedisFlusher {
+	rf := new(RedisFlusher)
+	rf.redis_pool = redis_pool
+	rf.implementation = implementation
+	return rf
+}
+
+func (this *RedisFlusher) Flush(to_flush batcher.Flushable) (err error) {
+	conn := this.redis_pool.Get()
+	this.implementation.Flush(conn, to_flush)
+	return err
+}
+
+type RedisPool interface {
+	Get() redis.Conn
+}
